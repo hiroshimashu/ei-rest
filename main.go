a6eb35dc29fe3bc548f8a9c4475fec83c1621446
hiroshimashu <hiroshimashu@gmail.com>
2020-05-22 02:59:26 +0900

    Implementing base user rest like end point

--- main.go
+++ main.go
@@ -9,16 +9,53 @@ import (
 
 type UserStore interface {
 	GetUserId(id string) int
+	RecordUser(id string)
 }
 
 type UserServer struct {
 	store UserStore
 }
 
+func NewInMemoryUserStore() *InMemoryUserStore {
+	return &InMemoryUserStore{map[string]int{}}
+}
+
+type InMemoryUserStore struct {
+	store map[string]int
+}
+
+func (i *InMemoryUserStore) GetUserId(id string) int {
+	return i.store[id]
+}
+
+func (i *InMemoryUserStore) RecordUser(id string) {
+	i.store[id]++
+}
+
 func (u *UserServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	user := strings.TrimPrefix(r.URL.Path, "/users/")
+	switch r.Method {
+	case http.MethodPost:
+		u.processUser(w, user)
+	case http.MethodGet:
+		u.showId(w, user)
+	}
+}
 
-	fmt.Fprint(w, u.store.GetUserId(user))
+func (u *UserServer) showId(w http.ResponseWriter, user string) {
+	userId := u.store.GetUserId(user)
+
+	if userId == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	fmt.Fprint(w, userId)
+
+}
+
+func (u *UserServer) processUser(w http.ResponseWriter, user string) {
+	u.store.RecordUser(user)
+	w.WriteHeader(http.StatusAccepted)
 }
 
 func GetUserId(id string) string {
@@ -34,7 +71,7 @@ func GetUserId(id string) string {
 }
 
 func main() {
-	server := &UserServer{}
+	server := &UserServer{NewInMemoryUserStore()}
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
 	}
--- main_test.go
+++ main_test.go
@@ -8,12 +8,17 @@ import (
 )
 
 type StubUserStore struct {
-	ids map[string]int
+	ids   map[string]int
+	users []string
 }
 
 func (s *StubUserStore) GetUserId(user string) int {
 	score := s.ids[user]
 	return score
+}
+
+func (s *StubUserStore) RecordUser(id string) {
+	s.users = append(s.users, id)
 }
 
 func TestGetUser(t *testing.T) {
@@ -22,6 +27,7 @@ func TestGetUser(t *testing.T) {
 			"1": 1,
 			"2": 2,
 		},
+		nil,
 	}
 	server := &UserServer{&store}
 	t.Run("returns user id 1", func(t *testing.T) {
@@ -29,6 +35,8 @@ func TestGetUser(t *testing.T) {
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusOK)
 		assertResponseBody(t, response.Body.String(), "1")
 	})
 
@@ -37,12 +45,78 @@ func TestGetUser(t *testing.T) {
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusOK)
 		assertResponseBody(t, response.Body.String(), "2")
 	})
+
+	t.Run("returns 404 on missing user", func(t *testing.T) {
+		request := newGetUserIdRequest("3")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusNotFound)
+	})
+}
+
+func TestStoreUser(t *testing.T) {
+	store := StubUserStore{
+		map[string]int{},
+		nil,
+	}
+
+	server := &UserServer{&store}
+
+	t.Run("it returns accepted on POST", func(t *testing.T) {
+		user := "1"
+		request := newPostUserRequest("1")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusAccepted)
+
+		if len(store.users) != 1 {
+			t.Errorf("got %d calls to RecordUsers want %d", len(store.users), 1)
+		}
+
+		if store.users[0] != user {
+			t.Errorf("did't record correct user got %q want %q", store.users[0], user)
+		}
+	})
+}
+
+func TestRecordUserAndRetrievingThem(t *testing.T) {
+	store := NewInMemoryUserStore()
+	server := &UserServer{store}
+	user := "1"
+
+	server.ServeHTTP(httptest.NewRecorder(), newPostUserRequest(user))
+	server.ServeHTTP(httptest.NewRecorder(), newPostUserRequest(user))
+	server.ServeHTTP(httptest.NewRecorder(), newPostUserRequest(user))
+
+	response := httptest.NewRecorder()
+	server.ServeHTTP(response, newGetUserIdRequest(user))
+	assertStatus(t, response.Code, http.StatusOK)
+
+	assertResponseBody(t, response.Body.String(), "3")
+}
+
+func assertStatus(t *testing.T, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("did not get correct status, got %d, want %d", got, want)
+	}
 }
 
 func newGetUserIdRequest(id string) *http.Request {
 	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/users/%s", id), nil)
+	return req
+}
+
+func newPostUserRequest(id string) *http.Request {
+	req, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("/users/%s", id), nil)
 	return req
 }
 
