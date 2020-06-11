75d780d563a3b50d5b77f0cb762ff4b9724929cb
hiroshimashu <hiroshimashu@gmail.com>
2020-06-11 22:05:00 +0900

    Add: movie_interactor implementation

---
+++ app/usecases/movie_interactor.go
@@ -0,0 +1,22 @@
+package usecases
+
+import "github.com/hiroshimashu/ei-rest/app/domain"
+
+type MovieInteractor struct {
+	MovieRepository MovieRepository
+}
+
+func (MovieInteractor *MovieInteractor) Index() (movies domain.Movies, err error) {
+	movies, err = MovieInteractor.MovieRepository.FindAll()
+	return
+}
+
+func (MovieInteractor MovieInteractor) IndexByID(id string) (movie domain.Movie, err error) {
+	movie, err = MovieInteractor.MovieRepository.FindByID(id)
+	return
+}
+
+func (MovieInteractor *MovieInteractor) Store(newMovie domain.Movie) (movie domain.Movie, err error) {
+	movie, err = MovieInteractor.MovieRepository.Save(newMovie)
+	return
+}
