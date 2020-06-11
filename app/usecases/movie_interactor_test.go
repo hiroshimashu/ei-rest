4f88f1e552e1003987bf043de27d4609854fea0b
hiroshimashu <hiroshimashu@gmail.com>
2020-06-11 21:49:13 +0900

    Add: mockMovieInteractor's store method test

--- app/usecases/movie_interactor_test.go
+++ app/usecases/movie_interactor_test.go
@@ -1,6 +1,7 @@
 package usecases
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 
@@ -22,7 +23,7 @@ func (mockMovieInteractor *MockMovieInteractor) IndexByID(id string) (movie doma
 }
 
 func (mockMovieInteractor *MockMovieInteractor) Store(newMovie domain.Movie) (movie domain.Movie, err error) {
-	movie, err = mockMovieInteractor.MovieRepository.Save(movie)
+	movie, err = mockMovieInteractor.MovieRepository.Save(newMovie)
 	return
 }
 
@@ -33,38 +34,40 @@ func NewMockMovieInteractor(mc *MockMovieRepository) *MockMovieInteractor {
 }
 
 type MockMovieRepository struct {
+	Movies domain.Movies
 }
 
 func (mr *MockMovieRepository) FindAll() (domain.Movies, error) {
-	return domain.Movies{
-		domain.Movie{
-			ID:  "3333",
-			URL: "https://example.com",
-		},
-		domain.Movie{
-			ID:  "5555",
-			URL: "https://example2.com",
-		},
-	}, nil
+	return mr.Movies, nil
 }
 
 func (mr *MockMovieRepository) FindByID(id string) (domain.Movie, error) {
-	if id == "5555" {
-		return domain.Movie{
-			ID:  "5555",
-			URL: "https://example.com",
-		}, nil
+	for _, v := range mr.Movies {
+		if v.ID == id {
+			return v, nil
+		}
 	}
 	return domain.Movie{}, nil
 }
 
 func (mr *MockMovieRepository) Save(movie domain.Movie) (domain.Movie, error) {
-
-	return domain.Movie{}, nil
+	mr.Movies = append(mr.Movies, movie)
+	return movie, nil
 }
 
 func TestMovieInteractor(t *testing.T) {
-	mr := &MockMovieRepository{}
+	mr := &MockMovieRepository{
+		Movies: domain.Movies{
+			domain.Movie{
+				ID:  "3333",
+				URL: "https://example.com",
+			},
+			domain.Movie{
+				ID:  "5555",
+				URL: "https://example2.com",
+			},
+		},
+	}
 	mockMovieInteractor := NewMockMovieInteractor(mr)
 
 	t.Run("Correctly Index method works", func(t *testing.T) {
@@ -86,5 +89,23 @@ func TestMovieInteractor(t *testing.T) {
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
 		}
+	})
+
+	t.Run("Correctly Store method works", func(t *testing.T) {
+		newMovie := domain.Movie{
+			ID:  "77777",
+			URL: "https://example3.com",
+		}
+
+		got, err := mockMovieInteractor.Store(newMovie)
+		if err != nil {
+			t.Error(err)
+		}
+		fmt.Println(mockMovieInteractor.Index())
+		want := newMovie
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+
 	})
 }
