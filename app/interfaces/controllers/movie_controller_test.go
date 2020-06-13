6f92bf849bbd798f62a0d6604b9a1d1df4312d91
hiroshimashu <hiroshimashu@gmail.com>
2020-06-13 21:30:17 +0900

    Add: test for mockMovieController's post method

--- app/interfaces/controllers/movie_controller_test.go
+++ app/interfaces/controllers/movie_controller_test.go
@@ -13,8 +13,8 @@ import (
 )
 
 func TestMovieController(t *testing.T) {
+	mockMovieController := NewMockMovieController()
 	t.Run("Correctly get movies", func(t *testing.T) {
-		mockMovieController := NewMockMovieController()
 		reqBody := bytes.NewBufferString("")
 		req := httptest.NewRequest(http.MethodGet, "/movies", reqBody)
 
@@ -38,6 +38,24 @@ func TestMovieController(t *testing.T) {
 
 		assertMovies(t, got, want)
 	})
+
+	t.Run("Correctly post movie", func(t *testing.T) {
+		reqBody := bytes.NewBufferString(`{"ID":"7777","URL":"https://example3.com"}`)
+		req := httptest.NewRequest(http.MethodPost, "/movies", reqBody)
+
+		res := httptest.NewRecorder()
+
+		mockMovieController.Create(res, req)
+		AssertStatus(t, res.Code, http.StatusOK)
+
+		got := getMovieFromResponse(t, res.Body)
+		want := domain.Movie{
+			ID:  "7777",
+			URL: "https://example3.com",
+		}
+
+		assertMovie(t, got, want)
+	})
 }
 
 func getMoviesFromResponse(t *testing.T, body io.Reader) (movies domain.Movies) {
@@ -52,7 +70,26 @@ func getMoviesFromResponse(t *testing.T, body io.Reader) (movies domain.Movies) 
 	return
 }
 
+func getMovieFromResponse(t *testing.T, body io.Reader) (movie domain.Movie) {
+	t.Helper()
+
+	err := json.NewDecoder(body).Decode(&movie)
+
+	if err != nil {
+		t.Fatal("unable to parse response from server")
+	}
+
+	return
+}
+
 func assertMovies(t *testing.T, got, want domain.Movies) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func assertMovie(t *testing.T, got, want domain.Movie) {
 	t.Helper()
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
